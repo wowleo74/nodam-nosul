4be928e40611548b4b26a5fe5cd33dd66b19ccd8
wowleo74 <wowleo74@gmail.com>
2026-03-17 13:40:22 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  console.log('Service Worker Active');
-});
-
-self.addEventListener('fetch', e => {});
